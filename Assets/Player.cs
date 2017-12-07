c079891479be987f78c76c88fdb367eb23b3b010
TylerDevers <devers.tyler@gmail.com>
2017-12-07 13:36:18 -0800

    ship now moves up down, left and right. TODO rotate about yaw axis

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour {
 
 	[Tooltip("in Meters/second")] [SerializeField] float xSpeed = 4f;
 	[Tooltip("In meters")] [SerializeField] float xRange = 3f;
-	
+	[Tooltip("in Meters/second")] [SerializeField] float ySpeed = 4f;
+	[Tooltip("In meters")] [SerializeField] float yRange = 2f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,9 +18,15 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		float yOffset = yThrow * ySpeed * Time.deltaTime;
 		float rawXPos = transform.localPosition.x + xOffset;
+		float rawYPos = transform.localPosition.y + yOffset;
 		float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+		float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange); 
+
 		transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+		transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
 	}
 }
