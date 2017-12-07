8763024f7d68aa38a51cecb3ddd797739470525a
TylerDevers <devers.tyler@gmail.com>
2017-12-07 12:10:58 -0800

    added ship and ability to move along x axis. added circuit

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Awake() 
+	{
+		DontDestroyOnLoad(this);
+	}
+	void Start () {
+		Invoke("LoadFirstScene",5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LoadFirstScene() 
+	{
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	[Tooltip("in Meters/second")] [SerializeField] float xSpeed = 4f;
+	[Tooltip("In meters")] [SerializeField] float xRange = 3f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		float rawXPos = transform.localPosition.x + xOffset;
+		float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+		transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+	}
+}
