015e9dcbc03dd81fe9820e4a6b9d4214ed76317e
TylerDevers <devers.tyler@gmail.com>
2018-01-18 12:04:09 -0800

    added ScoreBoard.cs script/class. Score now changes when enemies die

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	ScoreBoard scoreBoard;
 	[SerializeField] GameObject deathFX;
 	[SerializeField] Transform parent;
-
+	[SerializeField] int scorePerHit = 12;
 	// Use this for initialization
-	void Start () 
-	{
+	void Start ()
+    {
+        AddBoxCollider();
+		scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
-		boxCollider.isTrigger = false;
-	}
+        boxCollider.isTrigger = false;
+    }
 
     void OnParticleCollision(GameObject other)
 	{
+		scoreBoard.ScoreHit(scorePerHit);
 		GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
 		Destroy(gameObject);
